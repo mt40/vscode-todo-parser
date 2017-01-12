1236cef116ac42bc618ca73a5256251e53e5bc64
Suyog Soti <suyog.soti@gmail.com>
2017-01-11 22:38:56 -0700

    undo comment commit to pass build test

--- src/types/TodoType.ts
+++ src/types/TodoType.ts
@@ -27,10 +27,9 @@ export class TodoType {
 
   getDisplayString(): string {
     let url = this.getFile().data.uri.toString();
-    let middle = "#"
-    // account for untitled files
+    let middle = "#";
     if (url.split(":")[0].toString() == "untitled") {
-        middle = "; Line Number: "
+        middle = "; Line Number: ";
     }
     let path = url + middle + this.getLineNumber().toString();
     return `From ${path}\n----------------------------------\n${this.getContent()}`;
